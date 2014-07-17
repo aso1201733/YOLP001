3913fc29288bde5631daca294fcf7488be865af8
student <student@PC633S06.fgakusei.local>
2014-07-17 11:17:46 +0900

    地図環境まで

---
+++ src/jp/ac/st/asojuku/yolp001/MainActivity.java
@@ -0,0 +1,39 @@
+package jp.ac.st.asojuku.yolp001;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity implements LocationListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+}
