9b8fd0d0f19d219ccaec4b94fe3ba658d80e3c04
student <student@PC111S41.fgakusei.local>
2014-07-25 10:54:07 +0900

    渋谷駅表示できたやつ

--- src/jp/ac/st/asojuku/yolp001/MainActivity.java
+++ src/jp/ac/st/asojuku/yolp001/MainActivity.java
@@ -1,17 +1,58 @@
 package jp.ac.st.asojuku.yolp001;
 
+import jp.co.yahoo.android.maps.GeoPoint;
+import jp.co.yahoo.android.maps.MapController;
+import jp.co.yahoo.android.maps.MapView;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity implements LocationListener {
 
+	LocationManager mLocationManager = null;
+	MapView mMapView = null;
+	int lastLatitube = 0;
+	int lastLongitube = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		mMapView = new MapView(this,
+				"dj0zaiZpPUJwdE1WSDcyaWNLbSZzPWNvbnN1bWVyc2VjcmV0Jng9MDU-");
+		mMapView.setBuiltInZoomControls(true);
+		mMapView.setScalebar(true);
+
+		double lat = 35.658516;
+		double lon = 139.701773;
+		GeoPoint gp = new GeoPoint((int) (lat * 1000000), (int) (lon * 1000000));
+
+		MapController c = mMapView.getMapController();
+
+		c.setCenter(gp);
+		c.setZoom(3);
+
+		setContentView(mMapView);
+
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+		String provider = mLocationManager.getBestProvider(criteria, true);
+
+		mLocationManager.requestLocationUpdates(provider, 0, 0, this);
 	}
 
 	@Override
@@ -23,6 +64,18 @@ public class MainActivity extends Activity implements LocationListener {
 
 	@Override
 	public void onLocationChanged(Location location) {
+		double lat = location.getLatitude();
+		int latitube = (int) (lat * 1000000);
+
+		double lon = location.getLongitude();
+		int longitube = (int) (lon * 1000000);
+
+		if(latitube/1000!=this.lastLatitube/1000 || longitube/1000 != this.lastLongitube/1000){
+			GeoPoint gp = new GeoPoint(latitube,longitube);
+			MapController c = mMapView.getMapController();
+
+			c.setCenter(gp);
+		}
 	}
 
 	@Override
